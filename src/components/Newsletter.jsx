a71dd0a7f3f67d3cd1fd0a47add286b30949c2d6
Torr1co <fabriziotorrico27@gmail.com>
2022-04-08 11:42:20 -0300

    first deploy

---
+++ src/components/Newsletter.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import styled from "styled-components";
+import { Send } from "@material-ui/icons";
+
+const Container = styled.div`
+  flex: 1;
+  margin: 5px;
+  min-width: 280px;
+  height: 350px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #f5fbfd;
+  position: relative;
+`;
+
+const Title = styled.h2``;
+const Desc = styled.p``;
+const InputContainer = styled.div``;
+const Input = styled.input``;
+const Button = styled.button``;
+const Newsletter = () => {
+  return (
+    <Container>
+      <Title>Newsletter</Title>
+      <Desc>Get timely updates from your favorite products.</Desc>
+      <InputContainer>
+        <Input placeholder="your email" />
+        <Button>
+          <Send />
+        </Button>
+      </InputContainer>
+    </Container>
+  );
+};
+
+export default Newsletter;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+import Announcement from "../components/Announcement";
+import Slider from "../components/Slider";
+import Categories from "../components/Categories";
+import Products from "../components/Products";
+import Newsletter from "../components/Newsletter";
+const Home = () => {
+  return (
+    <div>
+      <Announcement />
+      <NavBar />
+      <Slider />
+      <Categories />
+      <Products />
+      <Newsletter />
+    </div>
+  );
+};
+
+export default Home;
